8e3ff58b2bd62103e555567931aa1231e684aa6b
koreyk <koreyk@slalom.com>
2018-12-09 01:47:17 -0500

    times

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { Marbles } from './dayNine';
 let starttime = moment();
 Marbles.getScore().then((result) => {
     let endtime = moment();
-    console.log("time took: " + endtime.diff(starttime, "milliseconds").toString());
+    console.log("time took: (milliseconds) " + endtime.diff(starttime, "milliseconds").toString());
+    console.log("time took: (seconds) " + endtime.diff(starttime, "seconds").toString());
     console.log("answer: " + result)
 });
