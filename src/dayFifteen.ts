3f972d0a1922db4a29cc57ddd36be87b6b953caf
koreyk <koreyk@slalom.com>
2018-12-18 00:01:16 -0500

    hells yeah, part 1 done

--- src/dayFifteen.ts
+++ src/dayFifteen.ts
@@ -14,7 +14,7 @@ export interface Creature {
 
 export class DnD {
     public static fs = bluebird.Promise.promisifyAll(require('fs'));
-    public static Creatures: { [id: string]: Creature } = {};
+    public static Creatures: Creature[] = [];
     public static Mine: string[][] = [];
 
     public static run = async (): Promise<string> => {
@@ -22,99 +22,81 @@ export class DnD {
         let final = "";
         let round = 0;
         let stop = false;
-        while (!stop && round < 30) {
+        let fullRound = true;
+        while (!stop && round < 50000) {
             round++;
+            DnD.Creatures.sort(DnD.sort);
+            DnD.Creatures.forEach((c) => {
+                if (c.hp < 1) {
+                    DnD.Mine[c.location.y][c.location.x] = ".";
+                    return;
+                }
 
-            for (let y = 0; y < DnD.Mine.length; y++) {
-                for (let x = 0; x < DnD.Mine[y].length; x++) {
-                    if (["", "#", "."].indexOf(DnD.Mine[y][x]) > -1) {
-                        continue;
+                let enemySpots: { e: Creature, s: Spot }[] = [];
+                let enemies = DnD.FindEnemies(c);
+                if (enemies.length === 0) {
+                    fullRound = false;
+                }
+                enemies.forEach((e) => {
+                    DnD.getOptions(e.location, c.location, DnD.Mine).forEach((s) => enemySpots.push({ e, s }));
+                });
+
+                let options: { e: Creature, s: Spot, p: Spot, distance: number }[] = [];
+                enemySpots.forEach((es) => {
+                    let paths = DnD.getPath(c, es.s);
+                    if (paths.distance === -1) {
+                        return;
                     }
-                    let c = DnD.Creatures[DnD.Mine[y][x]];
-                    if (c.hp < 1) {
-                        DnD.Mine[y][x] = '.';
-                        delete DnD.Creatures[c.id];
+                    options.push({ e: es.e, s: es.s, p: paths.path, distance: paths.distance });
+                });
+
+                if (options.length === 0) {
+                    return;
+                }
+
+                options.sort((a, b) => {
+                    let d = a.distance - b.distance;
+                    if (d !== 0) {
+                        return d;
                     }
-
-                    if (c.round === round) {
-                        continue;
+                    let y = a.s.y - b.s.y;
+                    if (y !== 0) {
+                        return y;
                     }
+                    return a.s.x - b.s.x;
+                });
 
-                    c.round = round;
+                DnD.Mine[c.location.y][c.location.x] = ".";
+                c.location = options[0].p;
+                DnD.Mine[c.location.y][c.location.x] = c.type;
 
-                    let ids = DnD.FindEnemies(c);
-                    if (ids.length === 0) {
-                        stop = true;
-                        break;
-                    }
-
-                    let bestStep = null as any;
-                    let bestE: Creature = null as any;
-                    let bestDistance = Number.MAX_SAFE_INTEGER;
-                    ids.forEach((e) => {
-                        let newE = DnD.Creatures[e];
-                        let paths = DnD.getPath(c, newE);
-                        if (paths.distance < 0) {
-                            return;
-                        }
-
-                        let best = paths.paths.sort(DnD.sort)[0];
-
-                        if (!bestE || paths.distance < bestDistance) {
-                            bestE = newE;
-                            bestStep = best;
-                            bestDistance = paths.distance;
-                            return;
-                        }
-                        if (bestDistance === paths.distance) {
-                            if (newE.hp < bestE.hp) {
-                                bestE = newE;
-                                bestStep = best;
-                                bestDistance = paths.distance;
-
-                            }
-                            return;
-                        }
-                        if (paths.distance !== bestDistance || newE.location.y > bestE.location.y) {
-                            return;
-                        }
-                        if (newE.location.y < bestE.location.y) {
-                            bestE = newE;
-                            bestStep = best;
-                            bestDistance = paths.distance;
-                            return;
-                        }
-                        if (newE.location.x < bestE.location.x) {
-                            bestE = newE;
-                            bestStep = best;
-                            bestDistance = paths.distance;
-                            return;
-                        }
+                let targets: Creature[] = DnD.getTargets(c, DnD.Mine).map((t) => {
+                    return DnD.Creatures.find((e) => {
+                        return e.location.y === t.y && e.location.x === t.x;
                     });
-
-                    if (!bestE) {
-                        continue;
-                    }
-
-                    if (bestStep && bestDistance > 0) {
-                        DnD.Mine[y][x] = '.';
-                        DnD.Mine[bestStep.y][bestStep.x] = c.id.toString();
-                        c.location = bestStep;
-                    }
-
-                    if (bestDistance < 2) {
-                        DnD.Creatures[bestE.id].hp -= 3;
-                        if (DnD.Creatures[bestE.id].hp < 1) {
-                            DnD.Mine[bestE.location.y][bestE.location.x] = '.';
-                            delete DnD.Creatures[bestE.id.toString()];
-                        }
-                    }
+                }).filter((e) => e) as Creature[];
+                if (targets.length === 0) {
+                    return;
                 }
+                targets.sort((a, b) => {
+                    let hp = a.hp - b.hp;
+                    if (hp !== 0) {
+                        return hp;
+                    }
+                    let y = a.location.y - b.location.y;
+                    if (y !== 0) {
+                        return y;
+                    }
+                    return a.location.x - b.location.x;
 
-                if (stop) {
-                    break;
+                });
+
+                targets[0].hp -= 3;
+
+                if (targets[0].hp < 1) {
+                    DnD.Mine[targets[0].location.y][targets[0].location.x] = ".";
                 }
-            }
+            });
 
             DnD.killDead();
             let types = new Set();
@@ -122,87 +104,105 @@ export class DnD {
             if ([...types].length === 1) {
                 stop = true;
             }
-
-            final += DnD.print(round) + "\n";
+            console.log(round);
+            await DnD.fs.writeFileAsync("input/dnd/" + round, DnD.print(round));
         }
 
-        await DnD.fs.writeFileAsync("input/dndMap.txt", final);
+        await DnD.fs.writeFileAsync("input/dndMap.txt", DnD.print(round));
         let hp = Object.entries(DnD.Creatures).reduce((sum, x) => sum + x[1].hp, 0);
-        round--;
+        if (!fullRound) {
+            round--;
+        }
         console.log("round: " + round.toString() + " hp: " + hp.toString());
         return (round * hp).toString();
     }
 
-    public static killDead = (): void => {
-        let ids = Object.keys(DnD.Creatures).filter((key) => {
-            return DnD.Creatures[key].hp < 1;
+    public static FindEnemies = (npc: Creature): Creature[] => {
+        return DnD.Creatures.filter((e) => {
+            if (e.hp < 1) {
+                DnD.Mine[e.location.y][e.location.x] = ".";
+                return false;
+            }
+            return e.type !== npc.type
         });
-
-        ids.forEach((key) => {
-            delete DnD.Creatures[key];
-        })
     }
 
-    public static getPath = (c: Creature, e: Creature): { distance: number, paths: Spot[] } => {
+    public static killDead = (): void => {
+        DnD.Creatures = DnD.Creatures.filter((e) => {
+            if (e.hp < 1) {
+                DnD.Mine[e.location.y][e.location.x] = ".";
+            }
+            return e.hp > 0;
+        });
+    }
+
+    public static getPath = (c: Creature, e: Spot): { distance: number, path: Spot } => {
+        if (c.location.x === e.x && c.location.y === e.y) {
+            return {
+                distance: 0,
+                path: e
+            }
+        }
+
         let mine = DnD.Mine.map((y) => {
             return y.map((x) => {
                 return x;
             })
         });
+        mine[e.y][e.x] = "1";
+        let neighbors = DnD.getOptions(e, c.location, mine);
 
-        let neighbors = DnD.getOptions(e.location, e.location, mine);
-        let step = 1;
-        while (neighbors.length > 0) {
+        let step = 2;
+        let foundNpc = false;
+        let added = new Set();
+        neighbors.forEach((x) => added.add(x.x + "," + x.y));
+        while (neighbors.length > 0 && !foundNpc) {
             neighbors.forEach((x) => {
+                if (c.location.x === x.x && c.location.y === x.y) {
+                    return;
+                }
                 mine[x.y][x.x] = step.toString();
             });
-            let newN: Spot[] = [];
+
+            let newS: Spot[] = [];
             neighbors.forEach((x) => {
-                DnD.getOptions(x, x, mine).forEach((y) => newN.push(y));
+                if (foundNpc) {
+                    return;
+                }
+                if (c.location.x === x.x && c.location.y === x.y) {
+                    foundNpc = true;
+                    return;
+                }
+                let ops = DnD.getOptions(x, c.location, mine);
+                ops.forEach((y) => {
+                    if (added.has(y.x + "," + y.y)) {
+                        return;
+                    }
+                    added.add(y.x + "," + y.y);
+                    newS.push(y);
+                });
             });
-            neighbors = newN;
+            neighbors = newS;
             step++;
         }
-        let best = -1;
-        let found: Spot[] = [];
-        neighbors = DnD.getRoutes(c.location, e.location, mine);
-        let actual = false;
-        neighbors.forEach((n) => {
-            let v = Number(mine[n.y][n.x]);
-            if (v === e.id) {
-                actual = true;
-            }
-            if (isNaN(v)) {
-                return;
+
+        neighbors = DnD.getRoutes(c, mine);
+
+        let best = neighbors.reduce((b, s) => {
+            let n = Number(mine[s.y][s.x]);
+            if (b === -1) {
+                return n;
             }
 
-            if (best === -1 || v < best) {
-                best = v;
-            }
-            found.push(n);
-        });
+            return n < b ? n : b;
+        }, -1);
 
-        if (actual) {
-            return {
-                distance: 0,
-                paths: []
-            };
-        }
-
-        found = found.filter((n) => mine[n.y][n.x] === best.toString());
-
+        let matches = neighbors.filter((s) => Number(mine[s.y][s.x]) === best);
+        matches.sort(DnD.sort);
         return {
             distance: best,
-            paths: found
-        };
-    }
-
-    public static FindNextStep = (npc: Creature, enemies: number[]) => {
-
-    }
-
-    public static FindEnemies = (npc: Creature): string[] => {
-        return Object.entries(DnD.Creatures).filter((e) => e[1].type !== npc.type).map((e) => e[0]);
+            path: best !== -1 ? matches[0] : null as any
+        }
     }
 
     public static sort = (a: Creature | { x: number, y: number }, b: Creature | { x: number, y: number }): number => {
@@ -247,59 +247,101 @@ export class DnD {
         return spots;
     }
 
-    public static getRoutes = (spot: Spot, target: Spot, mine: string[][]): Spot[] => {
+    public static getRoutes = (creature: Creature, mine: string[][]): Spot[] => {
+        let spot = creature.location;
         let spots: Spot[] = [];
         let y = spot.y;
         let x = spot.x;
 
         let match = (o: Spot) => {
-            return o.x === target.x && o.y === target.y;
+            if (!mine[o.y]) {
+                return false;
+            }
+
+            if (!mine[o.y][o.x]) {
+                return false;
+            }
+            let b = Number(mine[o.y][o.x]);
+
+            if (isNaN(b)) {
+                return false;
+            }
+
+            return true;
         }
 
         //down
-        if (mine[y + 1] || match({ y: y + 1, x })) {
-            let b = Number(mine[y + 1][x]);
-            if (!isNaN(b) && b > -1) {
-                spots.push({ x: x, y: y + 1 });
-            }
+        if (match({ y: y + 1, x })) {
+            spots.push({ x: x, y: y + 1 });
         }
 
         //right
-        if (mine[y][x + 1] || match({ y, x: x + 1 })) {
-            let b = Number(mine[y][x + 1]);
-            if (!isNaN(b) && b > -1) {
-                spots.push({ x: x + 1, y: y });
-            }
+        if (match({ y, x: x + 1 })) {
+            spots.push({ x: x + 1, y: y });
         }
 
         //left
-        if (mine[y][x - 1] || match({ y, x: x - 1 })) {
-            let b = Number(mine[y][x - 1]);
-            if (!isNaN(b) && b >-1) {
-                spots.push({ x: x - 1, y: y });
-            }
+        if (match({ y, x: x - 1 })) {
+            spots.push({ x: x - 1, y: y });
         }
 
         //up
-        if (mine[y - 1] && (mine[y - 1][x] || match({ y: y - 1, x }))) {
-            let b = Number(mine[y - 1][x]);
-            if (!isNaN(b) && b > -1) {
-                spots.push({ x: x, y: y - 1 });
-            }
+        if (match({ y: y - 1, x })) {
+            spots.push({ x: x, y: y - 1 });
         }
         return spots;
     }
 
+    public static getTargets = (creature: Creature, mine: string[][]): Spot[] => {
+        let spot = creature.location;
+        let spots: Spot[] = [];
+        let y = spot.y;
+        let x = spot.x;
+
+        let match = (o: Spot) => {
+            if (!mine[o.y]) {
+                return false;
+            }
+            if (!mine[o.y][o.x]) {
+                return false;
+            }
+            let c = mine[o.y][o.x];
+            if (c === "." || c === "#" || c === creature.type) {
+                return false;
+            }
+            return true;
+        }
+
+        //down
+        if (match({ y: y + 1, x })) {
+            spots.push({ x: x, y: y + 1 });
+        }
+
+        //right
+        if (match({ y, x: x + 1 })) {
+            spots.push({ x: x + 1, y: y });
+        }
+
+        //left
+        if (match({ y, x: x - 1 })) {
+            spots.push({ x: x - 1, y: y });
+        }
+
+        //up
+        if (match({ y: y - 1, x })) {
+            spots.push({ x: x, y: y - 1 });
+        }
+        return spots;
+    }
 
     public static print = (round: number): string => {
-        let out = DnD.Mine.map((y) => {
+        let out = DnD.Mine.map((y, i) => {
             let row = y.join('');
             let hp: string[] = [];
-            Object.entries(DnD.Creatures).forEach((c) => {
-                if (row.indexOf(c[0]) > -1) {
-                    hp.push(c[1].type + "(" + c[1].hp.toString() + ")");
+            DnD.Creatures.forEach((c) => {
+                if (i === c.location.y) {
+                    hp.push(c.type + "(" + c.hp.toString() + ")");
                 }
-                row = row.replace(c[0], c[1].type);
             })
             return row + "   " + hp.join(",");
         });
@@ -311,12 +353,12 @@ export class DnD {
     public static getMine = async (): Promise<string[][]> => {
         let raw: string = await DnD.fs.readFileAsync("input/dayFifteen.txt", { encoding: "utf8" });
         let cleaned = raw.split("\n");
-        let e = -10;
+        let e = 0;
         let map: string[][] = cleaned.map((row, y) => {
             let yv = row.split('');
             return yv.map((xv, x) => {
                 if (xv === "E" || xv === "G") {
-                    e--;
+                    e++;
                     let c: Creature = {
                         hp: 200,
                         type: xv,
@@ -324,8 +366,7 @@ export class DnD {
                         round: 0,
                         location: { x: x, y }
                     };
-                    DnD.Creatures[e.toString()] = c;
-                    return e.toString();
+                    DnD.Creatures.push(c);
                 }
                 return xv;
             });
