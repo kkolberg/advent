204cad8cd59c8b5823d85e0940dcfb62451cc3d2
koreyk <koreyk@slalom.com>
2018-12-26 14:35:19 -0500

    stuff

--- src/24.ts
+++ src/24.ts
@@ -1,7 +1,7 @@
 import * as bluebird from 'bluebird';
 
 export interface Army {
-    id: number;
+    id: string;
     hp: number;
     units: number;
     init: number;
@@ -21,21 +21,45 @@ export interface Body {
     Infection: Side;
 }
 export class Battle {
+    public static boost = 35;
     public static fs = bluebird.Promise.promisifyAll(require('fs'));
     public static body: Body = {} as any;
     public static run = async (): Promise<string> => {
         Battle.body = await Battle.getProgram();
-        let immuneIds: number[] = Battle.getIds(Battle.body.Immune.armies);
-        let infectionIds: number[] = Battle.getIds(Battle.body.Infection.armies);
-
-
-        while (immuneIds.length > 0 && infectionIds.length > 0) {
+        let immuneIds: string[] = Battle.getIds(Battle.body.Immune.armies);
+        let infectionIds: string[] = Battle.getIds(Battle.body.Infection.armies);
+        let deads: string[] = [];
+        let battled = true;
+        while (immuneIds.length > 0 && infectionIds.length > 0 && battled) {
+            //console.log("")
+            //console.log("------------")
+            //console.log("")
+            //console.log("Immune System:");
+            Battle.body.Immune.armies.forEach((e) => {
+                //console.log(e.id + " contains " + e.units.toString() + " units effective: " + Battle.getEffectivePower(e).toString());
+            });
+            //console.log("");
+            //console.log("Infection System:");
+            Battle.body.Infection.armies.forEach((e) => {
+                //console.log(e.id + " contains " + e.units.toString() + " units effective: " + Battle.getEffectivePower(e).toString());
+            });
+            //console.log("")
+            battled = false;
             let allids = immuneIds.concat(infectionIds);
-            allids.sort(Battle.sortTargeting)
-            let matched: number[] = [];
+            // let eFs = allids.map((m) => {
+            //     let a = Battle.findArmy(m);
+            //     return a.id + ":" + Battle.getEffectivePower(a).toString();
+            // }).join(",")
+            // //console.log("Eff powers: " + eFs);
+            allids.sort(Battle.sortTargeting);
+
+            let matched: string[] = [];
             let fighting: { att: Army, def: Army }[] = allids.map((id) => {
+
                 let att = Battle.findArmy(id);
                 let def = Battle.findTarget(att, matched);
+
+                //console.log(att.id + " picks " + (def ? def.id : " no one"));
                 if (!def) {
                     return { att, def }
                 }
@@ -43,25 +67,96 @@ export class Battle {
                 return { att, def }
             });
 
+            fighting.sort((a, b) => {
+                return b.att.init - a.att.init;
+            });
+
+            //console.log("  ")
+            //console.log("  ")
+
+            fighting.forEach((battle) => {
+                if (!battle.def) {
+                    return;
+                }
+
+                if (deads.indexOf(battle.att.id) > -1) {
+                    return;
+                }
+                let u = battle.def.units;
+                Battle.dealDamage(battle.att, battle.def);
+
+                if (battle.def.units < 1) {
+                    deads.push(battle.def.id);
+                }
+                if (u !== battle.def.units) {
+                    battled = true;
+                }
+
+
+            });
+
+            Battle.body.Immune.armies = Battle.body.Immune.armies.filter((x) => {
+                let alive = x.units > 0 && deads.indexOf(x.id) === -1;
+                if (!alive) {
+                    //console.log("========= " + x.id + " =========")
+                }
+                return alive;
+            });
+
+            Battle.body.Infection.armies = Battle.body.Infection.armies.filter((x) => {
+                let alive = x.units > 0 && deads.indexOf(x.id) === -1;
+                if (!alive) {
+                    //console.log("========= " + x.id + " =========")
+                }
+                return alive;
+            });
+
             immuneIds = Battle.getIds(Battle.body.Immune.armies);
             infectionIds = Battle.getIds(Battle.body.Infection.armies);
         }
 
+        let answer = 0;
+        Battle.body.Immune.armies.forEach((e) => {
+            answer += e.units;
+        });
+        console.log("Immune: " + answer.toString());
+
+        answer = 0;
+        Battle.body.Infection.armies.forEach((e) => {
+            answer += e.units;
+        });
+        console.log("Infection: " + answer.toString());
+        // if (Battle.body.Immune.armies.length > 0) {
+        //     Battle.body.Immune.armies.forEach((a) => {
+        //         answer += a.units;
+        //     });
+        // } else {
+        //     Battle.body.Infection.armies.forEach((a) => {
+        //         answer += a.units;
+        //     });
+        // }
+
         return "";
     }
 
-    public static dealDamage = (att: Army, def: Army): boolean => {
+    public static dealDamage = (att: Army, def: Army): void => {
+        let damage = Battle.CalDamage(att, def);
+        let d = Math.floor(damage / def.hp);
+        if (d > def.units) {
+            d = def.units;
+        }
 
-        return false;
+        //console.log(att.id + " attacks " + def.id + " for " + damage.toString() + " damage, killing " + d.toString() + " units");
+        def.units = def.units - d;
     }
 
-    public static findTarget = (att: Army, not_valid: number[]): Army => {
-        let defs = att.side === "immune" ? Battle.body.Infection : Battle.body.Immune;
+    public static findTarget = (att: Army, not_valid: string[]): Army => {
+        let defs = att.side === "Immune" ? Battle.body.Infection : Battle.body.Immune;
         let bestDamage = 0;
         let bestD: Army = null as any;
 
         defs.armies.forEach((d) => {
-            if (not_valid.indexOf(d.id)) {
+            if (not_valid.indexOf(d.id) > -1) {
                 return;
             }
             let damage = Battle.CalDamage(att, d);
@@ -93,6 +188,9 @@ export class Battle {
             }
         });
 
+        if (bestDamage === 0) {
+            return null as any;
+        }
         return bestD as any;
     };
 
@@ -109,11 +207,11 @@ export class Battle {
         return aE;
     }
 
-    public static getIds = (armies: Army[]): number[] => {
+    public static getIds = (armies: Army[]): string[] => {
         return armies.map((a) => a.id);
     }
 
-    public static sortTargeting = (one: number, two: number): number => {
+    public static sortTargeting = (one: string, two: string): number => {
         let a = Battle.findArmy(one);
         let b = Battle.findArmy(two);
 
@@ -127,7 +225,7 @@ export class Battle {
         return b.init - a.init;
     }
 
-    public static findArmy = (id: number): Army => {
+    public static findArmy = (id: string): Army => {
         let all = Battle.body.Immune.armies.concat(Battle.body.Infection.armies);
         return all.find((a) => {
             return a.id === id
@@ -154,20 +252,28 @@ export class Battle {
         let side = "";
         let id = 0;
         rows.forEach((row) => {
+            if (!row.trim()) {
+                return;
+            }
             if (row === "Immune System:") {
                 side = "Immune";
+                id = 1;
                 return;
             }
             if (row === "Infection:") {
                 side = "Infection"
+                id = 1;
                 return;
             }
             let unitsOther = row.split(" units each with ");
             let units = Number(unitsOther[0].trim());
-            let hpOthers = unitsOther[0].split(" hit points ");
+            let hpOthers = unitsOther[1].split(" hit points ");
             let hp = Number(hpOthers[0].trim());
 
             let weaknessOthers = hpOthers[1].split(")");
+            if (weaknessOthers.length === 1) {
+                weaknessOthers.unshift("");
+            }
             let weaknesses = weaknessOthers[0].replace("(", "").trim().split(";");
 
             let weak: string[] = [];
@@ -190,15 +296,15 @@ export class Battle {
             });
 
             let damageOther = weaknessOthers[1].split(" damage at initiative ");
-            let init = Number(damageOther[1].trim())
-            let damageStuff = damageOther[0].replace(" with an attack that does ", "").trim().split(" ");
+            let init = Number(damageOther[1].trim());
+            let damageStuff = damageOther[0].replace("with an attack that does ", "").trim().split(" ");
             let damage = Number(damageStuff[0].trim());
             let damageType = damageStuff[1].trim();
 
             (body as any)[side].armies.push({
-                id,
+                id: side === "Immune" ? "M" + id.toString() : "N" + id.toString(),
                 init,
-                damage,
+                damage: side === "Immune" ? damage + Battle.boost : damage,
                 damageType,
                 hp,
                 units,
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import moment from 'moment';
 
-import { Cave } from './22';
+import { Battle } from './24';
 
 let starttime = moment();
-Cave.run().then((result) => {
+Battle.run().then((result) => {
     let endtime = moment();
     console.log("time took: (milliseconds) " + endtime.diff(starttime, "milliseconds").toString());
     console.log("time took: (seconds) " + endtime.diff(starttime, "seconds").toString());
